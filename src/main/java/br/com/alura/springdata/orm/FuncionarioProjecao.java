b003070367297dba0191ec1323122cdf23876f31
Anderson Meurer <andersonmeurerr@gmail.com>
2021-10-17 21:46:19 -0300

    Revert "Revert "Criado""
    This reverts commit e70498df42d571be73ecf2aad475e3e0e70df1ec.

--- src/main/java/br/com/alura/springdata/orm/FuncionarioProjecao.java
+++ src/main/java/br/com/alura/springdata/orm/FuncionarioProjecao.java
@@ -1,5 +1,9 @@
 package br.com.alura.springdata.orm;
 
+/*
+ * Interface based Projection.
+ * Como alternativa, podemos também usar uma classe DTO com o mesmo propósito
+ */
 public interface FuncionarioProjecao {
 
 	Integer getId();
